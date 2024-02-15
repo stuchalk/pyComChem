fe9d0bc9468df28968695ff51441400e678f157a
Stuart Chalk <schalk@unf.edu>
2024-02-15 08:22:52 -0500

    formatting changes

--- example.py
+++ example.py
@@ -7,16 +7,19 @@ def props(casrn='57-83-0'):
     cmpd = detail(casrn, "properties")
     return cmpd
 
+
 def trifl():
     # gets a list of all compounds starting with "trifluoro*"
     hits = query("trifluoro*")
     print(hits)
 
-def key2cas(key):
+
+def key2casrn(key):
     # convert a substance CASRN into its InChIKey
     # uses code to find the lowest molecular weight hit for the InChIkey
     casrn = key2cas(key)
     print(casrn)
+
 
 def cas2png(casrn='57-83-0'):
     # create a png of a molecule based on its CASRN (via SVG from ComChem)
@@ -24,8 +27,7 @@ def cas2png(casrn='57-83-0'):
     print('image saved to ' + casrn + '.py')
 
 
-
-subs = ['57-83-0','71-43-2','118-96-7']
+subs = ['57-83-0', '71-43-2', '118-96-7']
 for sub in subs:
     out = props(sub)
     print(json.dumps(out, indent=4))
